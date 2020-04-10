d1aebd4bc133844845bdc17a5fa07f35e52f5875
Tania Mahajan <Tania.Mahajan@nagarro.com>
2020-04-10 21:15:08 +0530

    added jaeger and mysql support

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -14,10 +14,8 @@ namespace UserService.Controllers
         [HttpGet("{id}")]
         public ActionResult<User> Get(int id)
         {
-            User user = new User();
-            user.Age = 24;
-            user.Email = "tania.mahajan@nagarro.com";
-            user.Name = "Tania Mahajan";
+            UserContext context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            User user = context.GetUser(id);
             return Ok(user);
         }
     }
--- User.cs
+++ User.cs
@@ -7,8 +7,10 @@ namespace UserService
 {
     public class User
     {
+        private UserContext userContext;
         public string Name { get; set; }
         public int Age { get; set; }
+        public int userId { get; set; }
         public string Email { get; set; }
     }
 }
